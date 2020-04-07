a501966fe7f12e32d2f5b76b1daa18fb6ca3fc35
Fan Lei <leifan833@msn.com>
2020-04-06 21:53:36 -0700

    deployed prototype

---
+++ precache-manifest.375a6d34ba1ff71a8679388ce1e5eb36.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41d6de6ca53c9674d8dc8764973ddb58",
+    "url": "/index.html"
+  },
+  {
+    "revision": "42199934d660b29c0844",
+    "url": "/static/js/2.c40fca49.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c40fca49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93b4fe30ba8b00725516",
+    "url": "/static/js/main.0f682e3f.chunk.js"
+  },
+  {
+    "revision": "f8c5a9746872c0a14489",
+    "url": "/static/js/runtime-main.d6879700.js"
+  }
+]);
