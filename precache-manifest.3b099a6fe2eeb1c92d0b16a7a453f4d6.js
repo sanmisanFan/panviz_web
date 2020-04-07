ea059ede3eae95df4bf4f25f2f93743e26c15490
Fan Lei <leifan833@msn.com>
2020-04-06 22:09:20 -0700

    update

---
+++ precache-manifest.3b099a6fe2eeb1c92d0b16a7a453f4d6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42e78a47c17b6411807fdaa8d5a558d8",
+    "url": "/panviz_web/index.html"
+  },
+  {
+    "revision": "42199934d660b29c0844",
+    "url": "/panviz_web/static/js/2.c40fca49.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/panviz_web/static/js/2.c40fca49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93b4fe30ba8b00725516",
+    "url": "/panviz_web/static/js/main.0f682e3f.chunk.js"
+  },
+  {
+    "revision": "91e5e79e3fee77a801e6",
+    "url": "/panviz_web/static/js/runtime-main.a396da06.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.375a6d34ba1ff71a8679388ce1e5eb36.js"
+  "/panviz_web/precache-manifest.3b099a6fe2eeb1c92d0b16a7a453f4d6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/panviz_web/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
